b9a1dea90a24068cb1554abb85d696fcd0374fd7
Kyahwill <knownbutnotloved@gmail.com>
2023-12-10 22:33:39 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import {basicSetup, EditorView} from 'codemirror';
+    import {getCM, vim} from "@replit/codemirror-vim"
+    import { onMount } from 'svelte';
+    let dom: HTMLElement; 
+    let view: EditorView;
+    let value = `
+    toDateString();
+    testint 
+        hello world
+testing hello world testing
+    `
+
+    onMount(() => { 
+        view = new EditorView({
+            doc:value,
+                extensions: [
+                    vim(),
+                    basicSetup,
+                ],
+                parent: dom 
+        })
+        console.log(vim())
+    })
+    
+$:{
+        if(view) {
+
+            console.log(view.state.doc.toString());
+        }
+    }
+</script>
+
+
+<div bind:this={dom} />
